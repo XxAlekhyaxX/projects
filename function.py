c670e22e513b2ad0bffda58fcc6e7e1f5b42eb3e
XxAlekhyaxX <96320347+XxAlekhyaxX@users.noreply.github.com>
2026-07-06 07:36:51 -0700

    Create function.py

---
+++ function.py
@@ -0,0 +1,7 @@
+def display(brand_name, model_name, price, rating):
+    print(f"Brand Name: {brand_name}")
+    print(f"Model Name: {model_name}")
+    print(f"Price: {price} US Dollars")
+    print(f"Rating: {rating}")
+
+display("Apple", "Iphone 17 Pro", 200, 4.3)
