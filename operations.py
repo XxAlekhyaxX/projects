0de8d304e656d6b48053087568757039dcf3e310
XxAlekhyaxX <96320347+XxAlekhyaxX@users.noreply.github.com>
2026-07-06 07:52:12 -0700

    Create operations.py

---
+++ operations.py
@@ -0,0 +1,24 @@
+def add(n1, n2):
+    result = n1 + n2
+    return result
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    if n2 == 0:
+        print("cannot divide with zero")
+        return None
+    return n1 / n2
+
+
+def power(n1, n2):
+    return n1**n2
+
+
